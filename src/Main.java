3830fce7fbdeb6bc47dd81d419a191024afbff42
Carles <ccatalandelacruz@iessonferrer.net>
2024-01-28 22:09:30 +0100

    personaa1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
+/**
+ * Clase principal que contiene el método main para comparar edades.
+ */
 public class Main {
+
+    /**
+     * Método principal que compara la edad de una persona con una edad predefinida.
+     *
+     * @param args Argumentos de la línea de comandos (no utilizado en este caso).
+     */
     public static void main(String[] args) {
-        int edad = 28;
-        String nombre = "Alumno";
-        Persona Dani=new Persona("Dani",35,"11234567A");
-        if (Persona.getEdad() > edad){
-            System.out.println(Persona.getNombre() + "es mayor que " + nombre);
-        } else if (Persona.getEdad() == edad){
-            System.out.println(Persona.getNombre() + " y " + nombre + " tienen la misma edad");
+        int edadComparacion = 28;
+
+        String nombreComparacion = "Alumno";
+
+        Persona dani = new Persona("Dani", 35, "11234567A");
+
+        int edadPersona = dani.getEdad();
+        String nombrePersona = dani.getNombre();
+
+        if (edadPersona > edadComparacion) {
+            System.out.println(nombrePersona + " es mayor que " + nombreComparacion);
+        } else if (edadPersona == edadComparacion) {
+            System.out.println(nombrePersona + " y " + nombreComparacion + " tienen la misma edad");
         } else {
-            System.out.println(Persona.getNombre() + "es menor que " + nombre);
-        };
+            System.out.println(nombrePersona + " es menor que " + nombreComparacion);
+        }
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -3,27 +3,34 @@ public class Persona {
     private static int edad;
     private  String DNI;
     public Persona(String nuevoNombre,int nuevaEdad,String nuevoDni){
-        nombre=nuevoNombre;
-        edad=nuevaEdad;
+        Nuevonombre=nuevoNombre;
+        Nuevaedad=nuevaEdad;
         DNI= nuevoDni;
     }
     public Persona(){}
-
+    /**
+     *@param no se pasa parametro
+     *@return devuelve Nuevonombre valor de nombre
+     */
 
 
     public static String getNombre(){
-        return nombre;
+        return this Nuevonombre;
     }
+    /**
+     *@param param1 se le pasa el valor del nuevo nombre
+     *@return no devuelve nada es un void
+     */
     public void setNombre(String nuevoNombre){
         nombre=nuevoNombre;
     }
     public static int getEdad(){
         return edad;
     }
-   public void setEdad(int nuevaEdad){
-       edad=nuevaEdad;
-   }
-   public String getDNI(){
+    public void setEdad(int nuevaEdad){
+        this Nuevaedad=nuevaEdad;
+    }
+    public String getDNI(){
         return DNI;
     }
 
